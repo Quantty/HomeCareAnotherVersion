d6db0541cee0f3a5358a4de09e656e2b682f505a
Unknown <andreimoisaa@mail.ru>
2018-05-07 13:31:39 +0200

    new proj

---
+++ Web2/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web2.Models;
+
+namespace Web2.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: Admin
+
+       
+        public ActionResult Index()
+        {
+            var Users = UserManager.Users;
+            ViewBag.Title = Users.First().UserName;
+            return View();
+        }
+        public ActionResult UserTable()
+        {
+            var Users = UserManager.Users;
+            
+            return View(Users);
+        }
+    }
+}
