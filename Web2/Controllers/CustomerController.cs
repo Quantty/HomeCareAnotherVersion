b14fba37305f13b1f09fd86dc8d383a9ac64e7aa
4li100k <rm.cehlar@gmail.com>
2018-05-24 11:16:05 +0200

    customer view added

--- Web2/Controllers/CustomerController.cs
+++ Web2/Controllers/CustomerController.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Web2.Models;
@@ -10,30 +12,72 @@ namespace Web2.Controllers
 {
     public class CustomerController : Controller
     {
-        DbWrapper DbLink = new DbWrapper();
-        // GET: Customer
-        public ActionResult Index()
+        private DbWrapper DBLink = new DbWrapper();
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
         {
-
-            var sched = DbLink.GetSchedules();
-            List<Schedule> schedule = new List<Schedule>();
-
-
-            return View();
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
         }
 
-        public ActionResult TaskList()
+        public List<ApplicationUser> getEmployees()
         {
-            var cont = new DbWrapper();
-            
-            //var Tasks = othermanager.Tasks;
-            var tasks = cont.GetTasks();
-            var schedules = cont.GetSchedules();
+            List<ApplicationUser> employeeUsers = new List<ApplicationUser>();
 
-            dynamic mymodel = new ExpandoObject();
-            mymodel.tasks = tasks;
-            mymodel.schedules = schedules;
-            return View(mymodel);
+            foreach (ApplicationUser user in UserManager.Users)
+            {
+                if (!user.Roles.First().Equals("Employee"))
+                {
+                    employeeUsers.Add(user);
+                }
+            }
+            return employeeUsers;
+        }
+
+        public List<ApplicationUser> getCustomers()
+        {
+            List<ApplicationUser> customersUsers = new List<ApplicationUser>();
+
+            foreach (ApplicationUser user in UserManager.Users)
+            {
+                if (!user.Roles.First().Equals("Customer"))
+                {
+                    customersUsers.Add(user);
+                }
+            }
+            return customersUsers;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var schedules = DBLink.GetSchedules();
+            var employeeUsers = getEmployees();
+            var customerUsers = getCustomers();
+
+            List<MixViewModel> scheduleList = new List<MixViewModel>();
+
+            foreach (var schedule in schedules)
+            {
+                MixViewModel mymodel = new MixViewModel();
+                var userE = await UserManager.FindByIdAsync(schedule.employee_Id + "");
+                var userC = await UserManager.FindByIdAsync(schedule.customer_Id + "");
+
+                mymodel.employee = userE;
+                mymodel.schedule = schedule;
+                mymodel.customer = userC;
+                mymodel.task = DBLink.getTaskById(schedule.task_Id);
+
+                //if the employee's id is equal to the id we selected in UserList
+                scheduleList.Add(mymodel);
+
+            }
+            return View(scheduleList);
         }
     }
 }
